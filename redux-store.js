a7c91078d231d4011483d8bc686eec4835746315
Tausif4171 <tausifkhan4171@gmail.com>
2022-02-13 13:22:50 +0530

    create reducers

--- redux-store.js
+++ redux-store.js
@@ -1,21 +1,57 @@
 // Action Creators
 
-const newBooking = (name, amount) =>{
+const newBooking = (name, amount) => {
     return {
-        type : "NEW_BOOKING",
-        payload : {
+        type: "NEW_BOOKING",
+        payload: {
             name,
             amount
         }
     }
 }
 
-const cancelBooking = (name, refundAmount) =>{
+const cancelBooking = (name, refundAmount) => {
     return {
         type: "CANCEL_BOOKING",
         payload: {
             name,
             refundAmount
         }
+    }
+}
+
+// Reducers
+
+const reservationHistory = (oldreservationList = [], action) => {
+    if (action.type === "NEW_BOOKING") {
+        return [...oldreservationList, action.payload];
+    }
+    else if (action.type === "CANCEL_BOOKING") {
+        return oldreservationList.filter((record) => {
+            return record.name !== action.payload.name;
+        })
+    }
+    else {
+        return oldreservationList;
+    }
+}
+
+const cancellationHistory = (oldcancellationList = [], action) => {
+    if (action.type === "CANCEL_BOOKING") {
+        return [...oldcancellationList, action.payload];
+    }
+    else {
+        return oldcancellationList;
+    }
+}
+const accounting = (totalMoney = 100, action) => {
+    if (action.type === "NEW_BOOKING") {
+        return totalMoney + action.payload.amount; // we have to pay initially some amount
+    }
+    else if (action.type === "CANCEL_BOOKING") {
+        return totalMoney - action.payload.refundAmount;
+    }
+    else {
+        return totalMoney;
     }
 }
